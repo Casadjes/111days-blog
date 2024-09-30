8848480f8a1869e30d48c11260fe8891522b8da3
casadjes <esmilcasado901@gmail.com>
2024-09-30 14:06:25 -0400

    creacion de constante current_day

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export const SITE_TITLE = "Reto 111 Días";
 export const SITE_DESCRIPTION = "blog de reto 111 dias en el cual escribimos sobre tecnologia";
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
+export const CURRENT_DAY = 5;
