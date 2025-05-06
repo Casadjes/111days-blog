442d853c03bfa402e907df1eec00f168ca8d7c00
casadjes <esmilcasado901@gmail.com>
2025-05-05 23:44:48 -0400

    Integracion de autenticacion con clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import clerk from "@clerk/astro";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
+  integrations: [tailwind(), clerk()],
   adapter: vercel(),
+  output: "server",
 
   markdown: {
     shikiConfig: {
